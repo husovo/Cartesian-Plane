8fe53edf4bf1d383513fbee8b81add92cf97ec30
husovo <hussein1hh.hh@gmail.com>
2026-06-06 05:56:39 +0100

    Added a drawPlane function

--- graph.c
+++ graph.c
@@ -26,21 +26,39 @@ Vector2 rotP(Vector2 p,float angle)
 	};
 }
 
+void drawPlane(void);
+
 
 int main()
 {
 	InitWindow(width,height,"plane");
 	SetTargetFPS(60);
 	float angle = 0.0f;
-	int stepH = origin.x;
-	int stepV = origin.y;
-	int maxunitX = (width/2)/s;
-	int maxunitY = (height/2)/s;
-
 	while(!WindowShouldClose())
 	{
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		drawPlane();
+		// draw stuff on the plane
+		DrawCircleV(plot((Vector2){3.0f,2.0f}),18.0f,RED);
+		DrawCircleV(plot((Vector2){-3.0f,4.0f}),15.0f,RED);
+		DrawCircleV(plot((Vector2){-5.0f,-5.0f}),10.0f,GREEN);
+	
+		angle += 0.01f;
+		
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
+}
+
+void drawPlane(void){
+	int stepH = origin.x;
+	int stepV = origin.y;
+	int winwidth = (width/2)/s;
+	int winheight = (height/2)/s;
+
+
 		// draw the cartesian plane
 		for(int x = 0; x <= width; x += s){
 			DrawLine(x,0,x,height,BLACK);
@@ -50,13 +68,13 @@ int main()
 			DrawLine(0,y,width,y,BLACK);
 		}
 		// number the horizonatal axis.
-		for(int i = 1; i <= maxunitX; i++)
+		for(int i = 1; i <= winwidth; i++)
 		{
 			DrawText(TextFormat("%d",i), stepH + i*s-5, stepV+s/2-15, 30, BLACK);	
 			DrawText(TextFormat("%d",-i), stepH - i*s-5, stepV+s/2-15, 30, BLACK);
 		}
 		// number the vertical axis
-		for(int i = 1; i <= maxunitY; i++)
+		for(int i = 1; i <= winheight; i++)
 		{
 			DrawText(TextFormat("%d",i), stepH-s/2-10, stepV - i*s-12, 30, BLACK);	
 			DrawText(TextFormat("%d",-i), stepH-s/2-20, stepV + i*s-12, 30, BLACK);	
@@ -64,15 +82,4 @@ int main()
 		// draw the x and y axis
 		DrawRectangle(0, height/2-2,width, 4, BLACK);
 		DrawRectangle(width/2-2,0,4,height,BLACK);	
-		// draw stuff on the plane
-		DrawCircleV(plot((Vector2){3.0f,2.0f}),18.0f,RED);
-		DrawCircleV(plot((Vector2){-3.0f,4.0f}),15.0f,RED);
-		DrawCircleV(plot((Vector2){-5.0f,-5.0f}),10.0f,GREEN);
-				
-		angle += 0.01f;
-		
-		EndDrawing();
-	}
-	CloseWindow();
-	return 0;
 }
