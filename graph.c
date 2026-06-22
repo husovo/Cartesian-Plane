16b7264dff59d86cf3f194c4728b98629b8ff17f
husovo <husovo@hotmail.com>
2026-06-22 21:09:32 +0100

    draw parabola added

--- graph.c
+++ graph.c
@@ -15,13 +15,15 @@ typedef struct line
 } line;
 
 const Vector2 origin = {width / 2.0f, height / 2.0f};
-const int s = 50; // lenght on 1 unit in pixels
+const int SCALE= 50; // lenght on 1 unit in pixels
 
 Vector2 plot(Vector2 p) // plots cartesain co-ordiantes to screen
-{						// O+P = P
+{					
+		// O+P = P
+
 	return (Vector2){
-		origin.x + (s * p.x),
-		origin.y - (s * p.y)};
+		origin.x + ( SCALE * p.x),
+		origin.y - ( SCALE * p.y)};
 }
 
 Vector2 rotatePoint(Vector2 p, float angle)
@@ -33,6 +35,7 @@ Vector2 rotatePoint(Vector2 p, float angle)
 
 void drawPlane(float);
 void drawCircle(Vector2 centre, float r);
+
 Vector2 lineDirection(line l1)
 {
 
@@ -43,6 +46,8 @@ Vector2 lineDirection(line l1)
 	float dy = (q.y - p.y);
 	return (Vector2){dx, dy};
 }
+
+
 void drawLine(line l1, float t)
 {
 	Vector2 p = {l1.P.x, l1.P.y};
@@ -54,12 +59,15 @@ void drawLine(line l1, float t)
 	// parametric representation P +  t(Q -  P).
 	Vector2 pline = {
 		p.x + t * dx,
-		p.y + t * dy};
+		p.y + t * dy
+	};
 	// from p to negative direction
 	Vector2 nline = {
 		p.x - t * dx,
-		p.y - t * dy};
-	DrawLineEx(plot(nline), plot(pline), 5.0f, RED);
+		p.y - t * dy
+	};
+
+	DrawLineEx(plot(nline), plot(pline), 3.0f, RED);
 	// find the x intercept using basic algebra
 	if (dy != 0)
 	{ // y(t) = p.y + t(dx,dy)
@@ -74,7 +82,14 @@ void drawLine(line l1, float t)
 		DrawCircleV(plot(y_intercept), 6.0f, BLUE);
 	}
 }
-
+void drawParabola(float a, float b, float c)
+{
+	for(float x = -10.0f; x< 10.0f;x+=0.01f)
+	{
+		float y = a*x*x + b*x +c;
+		DrawCircleV(plot((Vector2){x,y}),2,PURPLE);
+	}
+}
 
 Vector2 pointOfIntersection(line l1, line l2)
 {
@@ -101,14 +116,17 @@ int main(void)
 	Camera2D camera;
 	camera.target=origin;
 	camera.offset= origin;
-	camera.zoom=1.0;
+	camera.zoom = 1.0;
 
 	//float angle = 0.0f;
-	line L1 = {{1.0f, -1.0f}, {3.0f, 5.0f}};
-	line L2 = {{2.0f, 1.0f}, {4.0f, -1.0f}};
+	
+	line L1 = {{0.5f, -0.5f}, {-1.5f, -4.5f}};
+	line L2 = {{-1.5f, 3.0f}, {-4.0f,1.0f}};
+
 	Vector2 x = pointOfIntersection(L1, L2);
 	Vector2 pos = plot(x);
 	float zoom_factor = 3;
+	int font_size = 20;
 	while (!WindowShouldClose())
 	{
 		if (camera.zoom > 3.0f) 
@@ -122,26 +140,28 @@ int main(void)
             delta = Vector2Scale(delta, -1.0f/camera.zoom);
             camera.target = Vector2Add(camera.target, delta);
         }
-		
-		if(IsKeyPressed(KEY_B)|| IsKeyPressedRepeat(KEY_B))
-			camera.zoom+=0.05;
-		
+		//zoom in
+		if(IsKeyPressed(KEY_EQUAL)|| IsKeyPressedRepeat(KEY_EQUAL)){
+			camera.zoom+=0.05f;
+		}
+		// zoom out
+		if(IsKeyPressed(KEY_MINUS) || IsKeyPressedRepeat(KEY_MINUS)){
+			camera.zoom-=0.05;
+			zoom_factor +=1.5;
+		};
+
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		BeginMode2D(camera);
 		
 		drawPlane(zoom_factor);
 		// draw stuff on the plane
-		if(IsKeyPressed(KEY_A) || IsKeyPressedRepeat(KEY_A)){
-			camera.zoom-=0.05;
-			zoom_factor +=0.5;
-		};
-
+		drawParabola(-2,3,-2);
 		drawLine(L1, 25);
 		drawLine(L2, 25);
 		DrawCircleV(pos, 6.0f, GREEN);
-		drawCircle((Vector2){3.0f,2.0f},3.0f);
-		DrawText(TextFormat("(%.2f,%.2f)",x.x,x.y),pos.x+s/2,pos.y-s/2,40,BLACK);	
+		//drawCircle((Vector2){3.0f,2.0f},3.0f);
+		DrawText(TextFormat("(%.2f,%.2f)",x.x,x.y),pos.x+SCALE/2,pos.y-SCALE/2,font_size,BLACK);	
 		EndMode2D();
 		EndDrawing();
 	}
@@ -172,16 +192,16 @@ void drawPlane(float zoom_factor)
 {
 	int stepH = origin.x;
 	int stepV = origin.y;
-	int winwidth = (width / 2) / s;
-	int winheight = (height / 2) / s;
+	int winwidth = (width / 2) / SCALE;
+	int winheight = (height / 2) / SCALE;
 	//Font my_font = LoadFont("NewCMMath-Regular.otf");
 	// draw the cartesian plane
-	for (int x = -zoom_factor*width; x <= zoom_factor*width; x += s)
+	for (int x = -zoom_factor*width; x <= zoom_factor*width; x += SCALE)
 	{
 		DrawLine(x, 0, x, zoom_factor*height, BLACK);
 		DrawLine(x, 0, x, -zoom_factor*height, BLACK);
 	}
-	for (int y= -zoom_factor*height; y <= zoom_factor*height; y += s)
+	for (int y= -zoom_factor*height; y <= zoom_factor*height; y += SCALE)
 	{
 		DrawLine(0, y, zoom_factor*width, y, BLACK);
 		DrawLine(0, y, -zoom_factor*width, y, BLACK);
@@ -190,27 +210,18 @@ void drawPlane(float zoom_factor)
 	// number the horizontal axis.
 	for (int i = 1; i <= winwidth*zoom_factor; i++)
 	{
-		DrawText(TextFormat("%d", i), stepH + i * s - 5, stepV + s / 2 - 15, 20, BLACK);
-		DrawText(TextFormat("%d", -i), stepH - i * s - 5, stepV + s / 2 - 15, 20, BLACK);
+		DrawText(TextFormat("%d", i), stepH + i *SCALE- 5, stepV +SCALE/ 2 - 15, 20, BLACK);
+		DrawText(TextFormat("%d", -i), stepH - i *SCALE- 5, stepV +SCALE/ 2 - 15, 20, BLACK);
 		
 	}
 	// number the vertical axis
 	for (int i = 1; i <= winheight*zoom_factor; i++)
 	{
-		DrawText(TextFormat("%d", i), stepH - s / 2 - 10, stepV - i * s - 12, 20, BLACK);
-		DrawText(TextFormat("%d", -i), stepH - s / 2 - 20, stepV + i * s - 12, 20, BLACK);
+		DrawText(TextFormat("%d", i), stepH -SCALE/ 2 - 10, stepV - i *SCALE- 12, 20, BLACK);
+		DrawText(TextFormat("%d", -i), stepH -SCALE/ 2 - 20, stepV + i *SCALE- 12, 20, BLACK);
 	}
 	// draw the x and y axis
-	DrawRectangle(-height*zoom_factor,height/2-2,width*zoom_factor*s,4,BLACK);
+	DrawRectangle(-height*zoom_factor,height/2-2,width*zoom_factor*SCALE,4,BLACK);
 	DrawRectangle(width/2-2,-height,4,zoom_factor*width,BLACK);
 	
 }
-
-//	line lineCircleIntersection(line l,float r)
-//	{
-//		const int t = 20;
-//		Vector2 dir = lineDirection(l);
-//		float x = l.P.x + t*dir.x;
-//		float y = l.P.y + t*dir.y;
-//	}
-
